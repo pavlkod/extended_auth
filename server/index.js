90fbfab9983db58dd7a06bfcc3b5c3161b177ae0
Pavel.K <pavelweb74@gmail.com>
2022-01-23 15:33:52 +0500

    Connect mongoDB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { config } from "dotenv";
+import mongoose from "mongoose";
 
 config();
 
 const app = express();
 
 (async () => {
+  await mongoose.connect(process.env.DB_URL);
   app.listen(process.env.PORT, () => console.log("Server started"));
 })();
