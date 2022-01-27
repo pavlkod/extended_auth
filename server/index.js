f009a89d6000a441aacd433731ceebde2327d1d7
Pavel.K <pavelweb74@gmail.com>
2022-01-27 21:29:50 +0500

    Add cookie-parser

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { config } from "dotenv";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 config();
 
 const app = express();
 
-app.use(express.json())(async () => {
+app.use(express.json());
+app.use(cookieParser());
+
+(async () => {
   await mongoose.connect(process.env.DB_URL);
   app.listen(process.env.PORT, () => console.log("Server started"));
 })();
