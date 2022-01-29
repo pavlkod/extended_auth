fd18a971722526fb81b43c6479d69a3de25ecdce
Pavel.K <pavelweb74@gmail.com>
2022-01-29 15:53:57 +0500

    Add User model

---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isActivated: {
+    type: Boolean,
+    default: false,
+  },
+  activatedLink: {
+    type: String,
+  },
+});
+
+export default model("User", userSchema);
