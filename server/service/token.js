f5f263843e821c17d6f666c2b5aeb0223a908f69
Pavel.K <pavelweb74@gmail.com>
2022-02-03 11:58:07 +0500

    Add saveToken in TokenService

--- server/service/token.js
+++ server/service/token.js
@@ -1,9 +1,20 @@
 import jwt from "jsonwebtoken";
+import TokenModel from "../models/Token.js";
+
 class TokenService {
   generateToken(payload) {
     const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_KEY, { expiresIn: "30m" });
     const refreshToken = jwt.sign(payload, process.env.JWT_ACCESS_KEY, { expiresIn: "30d" });
     return { accessToken, refreshToken };
+  }
+  async saveToken(userId, refreshToken) {
+    const tokenData = await TokenModel.findOne({ user: userId });
+    if (tokenData) {
+      tokenData.refreshToken = refreshToken;
+      return tokenData.save();
+    }
+    const token = TokenModel.create({ user: userId, refreshToken });
+    return token;
   }
 }
 export default new TokenService();
