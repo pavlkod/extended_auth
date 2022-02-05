fe1c8e42bb394421257c1d8a185ceb7a06191b4f
Pavel.K <pavelweb74@gmail.com>
2022-02-05 22:45:06 +0500

    Refactoring register method in UserService

--- server/service/user.js
+++ server/service/user.js
@@ -1,20 +1,21 @@
-import User from "../models/User.js";
+import UserModel from "../models/User.js";
 import bcryptjs from "bcryptjs";
-import uuid from "uuid";
 import mailService from "../service/mail.js";
+import { v4 } from "uuid";
 import tokenService from "../service/token.js";
 import UserDto from "../dto/user.js";
 
 class UserService {
   async register(email, password) {
-    const candidate = await User.findOne({ email });
+    const candidate = await UserModel.findOne({ email });
     if (candidate) {
       throw new Error("User already exists");
     }
     const hashPassword = await bcryptjs.hash(password, 3);
-    const activatedLink = uuid.v4();
-    const user = await User.create({ email, password: hashPassword, activatedLink });
-    await mailService.sendActivationMain(email, activatedLink);
+    const activatedLink = v4();
+
+    const user = await UserModel.create({ email, password: hashPassword, activatedLink });
+    await mailService.sendActivationMail(email, activatedLink);
 
     const userDto = new UserDto(user);
     const tokens = tokenService.generateToken({ ...userDto });
