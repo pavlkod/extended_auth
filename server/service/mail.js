a7236a6f44d6ffcae43ee0eb2a1732a9a43d47c2
Pavel.K <pavelweb74@gmail.com>
2022-02-06 15:39:44 +0500

    Add constructor in MailService

--- server/service/mail.js
+++ server/service/mail.js
@@ -1,4 +1,16 @@
+import nodemailer from "nodemailer";
 class MailService {
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+  }
   async sendActivationMail(to, link) {}
 }
 export default new MailService();
