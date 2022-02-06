5e9d2e1ec8f824a991ad26a238ecacad18d9c221
Pavel.K <pavelweb74@gmail.com>
2022-02-06 14:20:16 +0500

    Add register method in UserController

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,9 +1,20 @@
+import userService from "../service/user.js";
 class UserController {
   async users(req, res, next) {
     try {
       res.json({ message: "Get users" });
     } catch (e) {
       res.status(400).json({ message: "Could not get users" });
+    }
+  }
+  async register(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = await userService.register(email, password);
+      res.cookie("refreshToken", user.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
+      res.json(user);
+    } catch (e) {
+      res.status(400).json({ message: e.message });
     }
   }
 }
