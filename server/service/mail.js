0a0bcdf0bd138f6203752648b9ae961ef19a0378
Pavel.K <pavelweb74@gmail.com>
2022-02-11 23:26:51 +0500

    Implement sendActivationMail

--- server/service/mail.js
+++ server/service/mail.js
@@ -1,16 +1,30 @@
 import nodemailer from "nodemailer";
 class MailService {
-  constructor() {
+  setTransport() {
     this.transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
       },
     });
   }
-  async sendActivationMail(to, link) {}
+  async sendActivationMail(to, link) {
+    this.setTransport();
+    await this.transporter.sendMail({
+      from: `Pavel K <${process.env.SMTP_USER}>`,
+      to,
+      subject: `Activation account on ${process.env.API_URL}`,
+      text: "",
+      html: `
+        <div>
+          <h1>For activation account goto link</h1>
+          <a href=${link}>${link}</a>
+        </div>
+      `,
+    });
+  }
 }
 export default new MailService();
