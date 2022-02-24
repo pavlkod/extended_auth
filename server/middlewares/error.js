bf9cec384474aa1aec2fcf1890e56eed2feb1853
Pavel.K <pavelweb74@gmail.com>
2022-02-24 23:25:44 +0500

    Change error as instanceof ApiError

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,6 +1,6 @@
 import ApiError from "../exceptions/apiError.js";
 export default function (err, req, res, next) {
-  if (err in ApiError) {
+  if (err instanceof ApiError) {
     return res.status(err.status).json({ message: err.message, errors: err.errors });
   }
   return res.status(500).json({ message: "Server error" });
