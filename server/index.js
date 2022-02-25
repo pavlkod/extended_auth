e2dd83482f4d65f0b7a5c12c96c3ff4eab216d0c
Pavel.K <pavelweb74@gmail.com>
2022-02-25 23:14:53 +0500

    Carry down error middleware

--- server/index.js
+++ server/index.js
@@ -14,9 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-app.use(errorMiddleware);
 
 app.use("/api", router);
+
+app.use(errorMiddleware);
 (async () => {
   await mongoose.connect(process.env.DB_URL);
   app.listen(process.env.PORT, () => console.log("Server started"));
