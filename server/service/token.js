bae2e14f4f1dcf500a41012d4efad8349915fa18
Pavel.K <pavelweb74@gmail.com>
2022-03-06 18:03:36 +0500

    Change secret key in generate token

--- server/service/token.js
+++ server/service/token.js
@@ -4,7 +4,7 @@ import TokenModel from "../models/Token.js";
 class TokenService {
   generateToken(payload) {
     const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_KEY, { expiresIn: "30m" });
-    const refreshToken = jwt.sign(payload, process.env.JWT_ACCESS_KEY, { expiresIn: "30d" });
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_KEY, { expiresIn: "30d" });
     return { accessToken, refreshToken };
   }
   async saveToken(userId, refreshToken) {
