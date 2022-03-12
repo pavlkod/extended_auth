b627499ba62120b3410909b6cf277f5dd71427da
Pavel.K <pavelweb74@gmail.com>
2022-03-12 17:55:36 +0500

    Rename method users to getUsers in users route

--- server/router/index.js
+++ server/router/index.js
@@ -15,6 +15,6 @@ router.post("/login", UserController.login);
 router.post("/logout", UserController.logout);
 router.get("/activate/:link", UserController.activate);
 router.get("/refresh", UserController.refresh);
-router.get("/users", UserController.users);
+router.get("/users", UserController.getUsers);
 
 export default router;
