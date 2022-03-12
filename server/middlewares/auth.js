8adbbf58be31a542bccf11ba26f03e1e84a04251
Pavel.K <pavelweb74@gmail.com>
2022-03-12 15:57:13 +0500

    Add authMiddleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,22 @@
+import ApiError from "../exceptions/apiError.js";
+import tokenService from "../service/token.js";
+export default function (err, req, res, next) {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      return next(ApiError.UnauthorizedError());
+    }
+    const accessToken = authorization.split(" ")[1];
+    if (!accessToken) {
+      return next(ApiError.UnauthorizedError());
+    }
+    const userData = tokenService.validateAccessToken(accessToken);
+    if (!userData) {
+      return next(ApiError.UnauthorizedError());
+    }
+    req.user = userData;
+    next();
+  } catch (e) {
+    return next(ApiError.UnauthorizedError());
+  }
+}
