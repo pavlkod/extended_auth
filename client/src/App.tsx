307d9dad5c5e35725900209d1a1f1fbc9ce4bed7
Pavel.K <pavelweb74@gmail.com>
2022-03-16 22:41:50 +0500

    Refactor App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { FC } from "react";
 import "./App.css";
 
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -14,6 +14,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
