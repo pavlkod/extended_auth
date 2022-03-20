c69a5becc5ba461a160554a1fd5dc758ffa1b2dc
Pavel.K <pavelweb74@gmail.com>
2022-03-20 17:16:09 +0500

    Add registration in AuthService

--- client/src/services/AuthService.ts
+++ client/src/services/AuthService.ts
@@ -6,4 +6,7 @@ export default class AuthService {
   static async login(email: string, password: string): Promise<AxiosResponse<AuthResponse>> {
     return api.post<AuthResponse>("/login", { email, password });
   }
+  static async registration(email: string, password: string): Promise<AxiosResponse<AuthResponse>> {
+    return api.post<AuthResponse>("/register", { email, password });
+  }
 }
