ee37b6d7672a05aaf16adc3033e56a3784981ebe
Pavel.K <pavelweb74@gmail.com>
2022-03-30 23:22:50 +0500

    Add store in LoginForm

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,8 +1,10 @@
-import React, { FC, useState } from "react";
+import React, { FC, useContext, useState } from "react";
+import { Context } from "..";
 
 const LoginForm: FC = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const { store } = useContext(Context);
   return (
     <div>
       <input type="text" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
