207d167f1778bc7fa43c03f2177dc35b94393bfe
Pavel.K <pavelweb74@gmail.com>
2022-04-02 00:20:06 +0500

    Add LoginForm

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,11 @@
 import React, { FC } from "react";
 import "./App.css";
+import LoginForm from "./components/LoginForm";
 
 const App: FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <LoginForm />
     </div>
   );
 };
