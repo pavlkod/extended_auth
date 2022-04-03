cb9c693ab01ff6f3750438afe0aa39b31697ad03
Pavel.K <pavelweb74@gmail.com>
2022-04-03 17:14:09 +0500

    Add chechAuth in first view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
-import React, { FC } from "react";
+import React, { FC, useContext, useEffect } from "react";
 import "./App.css";
 import LoginForm from "./components/LoginForm";
 
+import { Context } from "./index";
+
 const App: FC = () => {
+  const { store } = useContext(Context);
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      store.chechAuth();
+    }
+  }, []);
   return (
     <div className="App">
       <LoginForm />
