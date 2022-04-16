af3827db17e90e7d83fc82d8104328798a65dd00
Pavel.K <pavelweb74@gmail.com>
2022-04-16 21:26:29 +0500

    Implement api.interceptors.response

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { AuthResponse } from "../models/response/AuthResponse";
 
 export const API_URL = "http://localhost:3030/api";
 
@@ -16,6 +17,19 @@ api.interceptors.response.use(
   config => {
     return config;
   },
-  err => {}
+  async err => {
+    const originalRequest = err.config;
+    if (err.response.status === 401 && err.config && !err.config._isRetry) {
+      originalRequest._isRetry = true;
+      try {
+        const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, { withCredentials: true });
+        localStorage.setItem("token", response.data.accessToken);
+        return api.request(originalRequest);
+      } catch (e) {
+        console.log("Unauthorized");
+      }
+    }
+    throw err;
+  }
 );
 export default api;
